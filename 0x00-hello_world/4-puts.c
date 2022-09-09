461011aae4c4fab0864717a6fd3ee7426d2b4e1f
Dany2j <floanflex@gmail.com>
2022-09-08 20:22:32 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#incluse <stdio.h>
+
+/**
+ * Description: main - Prints outs a string using the put function only.
+ * Return: 0
+ */
+void main()
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
