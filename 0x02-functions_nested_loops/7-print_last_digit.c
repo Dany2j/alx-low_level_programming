c1652905b77d614b10edda87b745b334d018dc2d
Dany2j <floanflex@gmail.com>
2022-09-14 03:47:06 -0700

    6-009

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit
+ *
+ * @n: interger
+ *
+ * Return: last digit.
+ */
+int print_last_digit(int n)
+{
+	int a;
+
+	a = n % 10;
+	if (a < 0)
+	{
+		_putchar(-a + 48);
+		return (-a);
+	}
+	else
+	{
+		_putchar(a + 48);
+		return (a);
+	}
+}
