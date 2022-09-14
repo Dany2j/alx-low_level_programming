fed1b047abf673ee6d1bac7ba388def12b3fbc80
Dany2j <floanflex@gmail.com>
2022-09-14 04:12:06 -0700

    update to 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,44 +8,43 @@
 int main(void)
 {
 	int c, boolean, boolean2;
-	long int d1, d2, fn, an2, d11, d22;
+	long int n1, n2, fn, fn2, n11, n22;
 
-	d1 = 1;
-	d2 = 2;
+	n1 = 1;
+	n2 = 2;
 	boolean =  boolean2 = 1;
-	printf("%ld, %ld", d1, d2);
+	printf("%ld, %ld", n1, n2);
 	for (c = 0; c < 96; c++)
 	{
 		if (boolean)
 		{
-			fn = d1 + d2;
+			fn = n1 + n2;
 			printf(", %ld", fn);
-			d1 = d2;
-			d2 = fn;
+			n1 = n2;
+			n2 = fn;
 		}
 		else
 		{
 			if (boolean2)
 			{
-				d11 = d1 % 1000000000;
-				d22 = d2 % 1000000000;
-				d1 = d1 / 1000000000;
-				d2 = d2 / 1000000000;
+				n11 = n1 % 1000000000;
+				n22 = n2 % 1000000000;
+				n1 = n1 / 1000000000;
+				n2 = n2 / 1000000000;
 				boolean2 = 0;
 			}
-			an2 = (d11 + d22);
-			fn = d1 + d2 + (fn2 / 1000000000);
+			fn2 = (n11 + n22);
+			fn = n1 + n2 + (fn2 / 1000000000);
 			printf(", %ld", fn);
-			printf("%ld", an2 % 1000000000);
-			d1 = d2;
-			d11 = d22;
-			d2 = fn;
-			d22 = (an2 % 1000000000);
+			printf("%ld", fn2 % 1000000000);
+			n1 = n2;
+			n11 = n22;
+			n2 = fn;
+			n22 = (fn2 % 1000000000);
 		}
-		if (((d1 + d2) < 0) && boolean == 1)
+		if (((n1 + n2) < 0) && boolean == 1)
 			boolean = 0;
 	}
 	printf("\n");
 	return (0);
 }
-
